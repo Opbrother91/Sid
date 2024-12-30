b5d85eacf7fc84b79229312d9267872d68ac5db6
Opbrother91 <shaikhrehan73828@gmail.com>
2024-12-30 22:44:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7838132287:AAEbzDoQB1LdWqV3ulQwE8ij3iriQdsaYwg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@shaikhrehan786590)
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5946419615")
+
+
+
+
+
+
+
+  
